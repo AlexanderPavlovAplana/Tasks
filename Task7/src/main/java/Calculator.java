ccd4bee0bd656df2145f89ff4ac428831ed14e83
AlexanderPavlovAplana <Alexander.Pavlov@aplana.com>
2018-10-20 15:04:39 +0300

    Task7

---
+++ Task7/src/main/java/Calculator.java
@@ -0,0 +1,33 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+
+        Application Calculate = new Application();
+            Calculate.getInput();
+        switch (Calculate.oper) {
+            case "+": {
+                Calculate.sum();
+                break;
+            }
+            case "-": {
+                Calculate.minus();
+                break;
+            }
+            case "*": {
+                Calculate.multiply();
+                break;
+            }
+            case "/": {
+                try {
+                    Calculate.divide();
+                } catch (Exeption mathExeption) {
+                    mathExeption.printStackTrace();
+                }
+                break;
+            }
+            default:
+                System.out.println(Calculate.oper + "не равно значениям */+-");
+        }
+    }
+}
